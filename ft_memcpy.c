7fada92e0f472634bb19896cac3ab48c9832c93f
CesarCRP97 <ccrp97@hotmail.es>
2026-05-28 15:14:43 +0200

    fix error memmove

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 12:18:59 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/27 11:18:06 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/05/28 15:10:35 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	char_dest = (unsigned char *) dest;
 	char_src = (unsigned char *) src;
 	i = 0;
+	if (dest == NULL && src == NULL)
+		return (NULL);
 	while (i < n)
 	{
 		char_dest[i] = char_src[i];
