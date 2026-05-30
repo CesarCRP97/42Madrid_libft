6b7a1723b5768b194ad72b206bbe686c82573d88
CesarCRP97 <ccrp97@hotmail.es>
2026-05-30 15:45:30 +0200

    add atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -3,14 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
+/*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 13:33:26 by cesar             #+#    #+#             */
-/*   Updated: 2026/05/30 13:33:44 by cesar            ###   ########.fr       */
+/*   Updated: 2026/05/30 15:44:55 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_atoi(const char *nptr)
 {
-	
+	int	i;
+	int	sign;
+	int	nbr;
+
+	i = 0;
+	sign = 1;
+	nbr = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign = -sign;
+		i++;
+	}
+	while (ft_isdigit(nptr[i]))
+	{
+		nbr = nbr * 10 + (nptr[i] - '0');
+		i++;
+	}
+	return (nbr * sign);
 }
