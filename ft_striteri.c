227e2eb268ceb5fa8ae97708d2d1141d4e1413e1
CesarCRP97 <32871629+CesarCRP97@users.noreply.github.com>
2026-06-01 10:28:38 +0200

    add striteri and strmapi

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,22 @@
 /*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 00:09:51 by cesar             #+#    #+#             */
-/*   Updated: 2026/05/31 00:10:18 by cesar            ###   ########.fr       */
+/*   Updated: 2026/06/01 10:28:13 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+	Applies the function 'f' to each character in the string 's'.
+*/
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
+	size_t	i;
 
+	i = 0;
+	while(s[i])
+	{
+		(f)(i, s);
+	}
 }
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,13 +6,30 @@
 /*   By: cesar <cesar@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/31 00:08:54 by cesar             #+#    #+#             */
-/*   Updated: 2026/05/31 00:09:17 by cesar            ###   ########.fr       */
+/*   Updated: 2026/06/01 10:25:13 by cesar            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+	Applies the function 'f' to each character of the string 's' and saves the
+	result in a new array 'new_s'.
+*/
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	size_t	len;
+	size_t	i;
+	char	*new_s;
+
+	i = 0;
+	len = ft_strlen(s);
+	new_s = malloc(sizeof(char) * (len + 1));
+	while (i <= len)
+	{
+		new_s[i] = (f)(i, s[i]);
+		i++;
+	}
+	new_s[i] = '\0';
+	return (new_s);
 }
