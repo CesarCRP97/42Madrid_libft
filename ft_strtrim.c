c1ecfb77aebece7a4cf25fbe3cb1b047f83bd182
CesarCRP97 <ccrp97@hotmail.es>
2026-06-01 15:52:42 +0200

    ooootra vez

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 20:35:27 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/30 20:53:06 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/06/01 14:43:11 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,30 @@
 
 /*
 	Parameters:
-		- s1: string to trim
-		- set: substring to be trimmed from s1.
+		- memchr cogiendo el set y haciendo un memchr con los caracteres
 */
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
 	size_t	end;
 	size_t	set_len;
-	size_t	i;
+	size_t	s1_len;
 
 	start = 0;
 	if (!s1)
 		return (NULL);
-	end = ft_strlen(s1);
+	if (!set)
+		return ((char *)s1);
+	s1_len = ft_strlen(s1)-1;
 	set_len = ft_strlen(set);
-	while (start < set_len && s1[start] == set[start])
+	end = s1_len - 1;
+	while (start < set_len && ft_memchr(set, s1[start], set_len) != NULL)
 	{
 		start++;
 	}
-	i = 0;
-	while (end >= 0 && s1[end] == set[i])
+	while (end >= 0 && ft_memchr(set, s1[end], set_len) != NULL)
 	{
 		end--;
-		i++;
 	}
 	return (ft_substr(s1, start, end - start));
 }
