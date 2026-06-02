d35bcdcbd0882dd188ec6e548242631f8237ae19
CesarCRP97 <ccrp97@hotmail.es>
2026-06-02 11:22:07 +0200

    fix strtrim now working

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/30 20:35:27 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/06/01 14:43:11 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/06/02 11:21:42 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,16 @@ char	*ft_strtrim(char const *s1, char const *set)
 		return (NULL);
 	if (!set)
 		return ((char *)s1);
-	s1_len = ft_strlen(s1)-1;
+	s1_len = ft_strlen(s1);
 	set_len = ft_strlen(set);
-	end = s1_len - 1;
-	while (start < set_len && ft_memchr(set, s1[start], set_len) != NULL)
+	while (start < s1_len && ft_memchr(set, s1[start], set_len) != NULL)
 	{
 		start++;
 	}
-	while (end >= 0 && ft_memchr(set, s1[end], set_len) != NULL)
+	end = s1_len - 1;
+	while (end >= start && ft_memchr(set, s1[end], set_len) != NULL)
 	{
 		end--;
 	}
-	return (ft_substr(s1, start, end - start));
+	return (ft_substr(s1, start, end - start + 1));
 }
